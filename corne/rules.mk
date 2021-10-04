9d95879db8f0f90f55d55dde5c8a1231a96a2e6d
Lin Xianyi <iynaix@gmail.com>
2021-10-04 18:13:28 +0800

    Add bongo cat animation for corne.

--- corne/rules.mk
+++ corne/rules.mk
@@ -1,4 +1,11 @@
 # elite-c uses dfu for flashing
 BOOTLOADER = atmel-dfu
+
+# save space on firmware
+LTO_ENABLE = yes
+MOUSEKEY_ENABLE = no
+
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306
+
+WPM_ENABLE = yes
